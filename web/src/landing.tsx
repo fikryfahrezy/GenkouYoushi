7d0b63074e405062241919e83054ef64ad60062b
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2026-08-29 12:58:35 +0700

    Update marketing page

---
+++ web/src/landing.tsx
@@ -0,0 +1,20 @@
+import { render } from "solid-js/web";
+import BookOpen from "lucide-solid/icons/book-open";
+import FileDown from "lucide-solid/icons/file-down";
+import Grid2x2 from "lucide-solid/icons/grid-2x2";
+import ImageUp from "lucide-solid/icons/image-up";
+import Pencil from "lucide-solid/icons/pencil";
+
+const icons = {
+  "book-open": BookOpen,
+  "file-down": FileDown,
+  grid: Grid2x2,
+  "image-up": ImageUp,
+  pencil: Pencil,
+};
+
+document.querySelectorAll<HTMLElement>("[data-lucide-icon]").forEach((element) => {
+  const name = element.dataset.lucideIcon as keyof typeof icons;
+  const Icon = icons[name];
+  if (Icon) render(() => <Icon aria-hidden="true" />, element);
+});
